2343c95747c52f0813547ce1a98bbf0d62227ac5
Muhammad Shehzad <m.shehzad0205@gmail.com>
2025-02-25 01:10:58 +0500

    Add remove duplicates and print sum of Array

---
+++ Array_Logic_Questions/remove_duplicates_and_sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main() {
+    
+    int sum = 0;
+    int size_of_array;
+    cout << "Enter Size of Array: ";
+    cin >> size_of_array;
+
+    vector<int> vec(size_of_array);
+
+    cout << "Enter Elements: ";
+    for(int i = 0; i < size_of_array; i++){
+        cin >> vec[i];
+    }
+    
+    set<int> uniqueSet(vec.begin(), vec.end());
+
+    cout << "Unique Array: ";
+    for(int num : uniqueSet){
+        cout << num << " ";
+        sum += num;
+    }
+
+    cout << endl;
+    cout << "Sum of unique Array: " << sum;
+    
+    return 0;
+}
